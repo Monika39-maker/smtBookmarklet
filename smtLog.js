fad73c0a0dfc66a7df2c705d458d32b6832acf09
Monika Dangol <60827831+Monika39-maker@users.noreply.github.com>
2024-05-12 12:18:17 +0100

    Update smtLog.js for minified
    Signed-off-by: Monika Dangol <60827831+Monika39-maker@users.noreply.github.com>

--- smtLog.js
+++ smtLog.js
@@ -1,11 +1,4 @@
-const FNZform = document.getElementById('aspnetForm');
-const logTypeField = document.getElementById("logtype");
-const priorityField = document.getElementById("logpriority");
-const componentField = document.getElementById("component");
-const summaryField = document.getElementById("logsummary");
-const detailField = document.getElementById("logdesc");
-const popUpbox = document.createElement('div');
-popUpbox.innerHTML = `<div>
+javascript:(function(){const FNZform=document.getElementById("aspnetForm");const logTypeField=document.getElementById("logtype");const priorityField=document.getElementById("logpriority");const componentField=document.getElementById("component");const summaryField=document.getElementById("logsummary");const detailField=document.getElementById("logdesc");const popUpbox=document.createElement("div");popUpbox.innerHTML=`<div>
 <h1 style="font-size: 25px;
 padding: 0px 0px 10px 40px;
 display: block;
@@ -29,28 +22,4 @@ margin: 0px;">
     <span>&nbsp;</span> 
     <input type="submit" class="button" value="Send" id="client-form-button"/> 
 </label>    
-</div>`
-
-FNZform.appendChild(popUpbox);
-var logReason
-const clientFormBtn = document.getElementById('client-form-button')
-FNZform.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    console.log('submitted')
-    logReason = document.getElementById('log-reason').value
-    console.log(logReason)
-    if (logReason == 'General') {
-        logTypeField.value = 'Request'
-        priorityField.value = 'High'
-        componentField.value = 'Other';
-        summaryField.value = 'A3 Reopen Accounts'
-        detailField.value = 'Please reopen the following accounts'
-    } else if (logReason == 'Complain') {
-        logTypeField.value = 'Complaints'
-        priorityField.value = 'Critical'
-        componentField.value = 'Other';
-        summaryField.value = 'Urgent'
-        detailField.value = 'Please raise complaint for the following accounts'
-    }
-})
-
+</div>`;FNZform.appendChild(popUpbox);var logReason;const clientFormBtn=document.getElementById("client-form-button");FNZform.addEventListener("submit",e=>{e.preventDefault();console.log("submitted");logReason=document.getElementById("log-reason").value;console.log(logReason);if(logReason=="General"){logTypeField.value="Request";priorityField.value="High";componentField.value="Other";summaryField.value="A3 Reopen Accounts";detailField.value="Please reopen the following accounts"}else if(logReason=="Complain"){logTypeField.value="Complaints";priorityField.value="Critical";componentField.value="Other";summaryField.value="Urgent";detailField.value="Please raise complaint for the following accounts"}});})()
